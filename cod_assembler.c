830b4be9aecd97e95748a671291c3811bcfde297
gabrielogh <gabriel.ogh@gmail.com>
2018-11-06 17:02:16 -0300

    Archivo cod_aseembler.c agregado

---
+++ cod_assembler.c
@@ -0,0 +1,27 @@
+#include "cod_intermedio.h"
+
+FILE *asm_code;
+
+void generar_codigo_assembler();
+
+void crear_archivo();
+
+
+void crear_archivo(){
+  asm_code = fopen("assembler.s","w");
+  if(asm_code == NULL){
+    printf("%s\n","Error al crear el archivo assembler.s" );
+  }
+}
+
+void generar_codigo_assembler(){
+  crear_archivo();
+
+  tresDirL *aux = head_td;
+
+  if( aux!= NULL){
+    while 
+
+  }
+
+}
